885817ff926acd4a3585c6f834dca27e0292672a
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2016-08-17 09:11:39 +0200

    entab

--- index.js
+++ index.js
@@ -113,25 +113,25 @@ function intersect(start0, end0, start1, end1) {
 
 // Function to compare Arrays of numbers. From http://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
 function equalArrays(array1, array2) {
-    // if the other array is a falsy value, return
-    if (!array1 || !array2)
-        return false;
+  // if the other array is a falsy value, return
+  if (!array1 || !array2)
+      return false;
 
-    // compare lengths - can save a lot of time
-    if (array1.length != array2.length)
-        return false;
+  // compare lengths - can save a lot of time
+  if (array1.length != array2.length)
+      return false;
 
-    for (var i = 0, l=array1.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (array1[i] instanceof Array && array2[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!equalArrays(array1[i],array2[i]))
-                return false;
-        }
-        else if (array1[i] != array2[i]) {
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;
-        }
-    }
-    return true;
+  for (var i = 0, l=array1.length; i < l; i++) {
+      // Check if we have nested arrays
+      if (array1[i] instanceof Array && array2[i] instanceof Array) {
+          // recurse into the nested arrays
+          if (!equalArrays(array1[i],array2[i]))
+              return false;
+      }
+      else if (array1[i] != array2[i]) {
+          // Warning - two different object instances will never be equal: {x:20} != {x:20}
+          return false;
+      }
+  }
+  return true;
 }
